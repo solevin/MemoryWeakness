fc56e746cd338bb7fb8a787ae429bfcc17e04e44
taiyo78910 <y.kambayashi78910@gmail.com>
2022-03-20 18:39:22 +0900

    #15: プレゼンス機能の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:memory_weakness/router.dart';
@@ -8,10 +9,12 @@ import 'package:memory_weakness/ui/play/play_setting_view.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:provider/provider.dart' as provider;
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseAuth.instance.signInAnonymously();
+  String uid = FirebaseAuth.instance.currentUser!.uid;
+  await updateUserPresence(uid);
   runApp(
     ProviderScope(
       child: provider.MultiProvider(
@@ -72,4 +75,19 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+}
+
+Future<void> updateUserPresence(String uid) async {
+  // プレゼンスをtrueに更新
+  await FirebaseDatabase.instance.ref(uid).update(
+    {
+      'presence': true,
+    },
+  );
+
+  // アプリの接続が切れ次第、プレゼンスをfalseに更新
+  await FirebaseDatabase.instance
+      .ref(uid)
+      .onDisconnect()
+      .update({'presence': false});
 }
