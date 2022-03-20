e55edf04276b9f7921e9b66e7fade8c8352bcf10
taiyo78910 <y.kambayashi78910@gmail.com>
2022-03-20 17:51:13 +0900

    #13: Firebase Authによる匿名認証

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:memory_weakness/router.dart';
@@ -10,6 +11,7 @@ import 'package:provider/provider.dart' as provider;
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseAuth.instance.signInAnonymously();
   runApp(
     ProviderScope(
       child: provider.MultiProvider(
